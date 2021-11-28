ad6944a040f8b8db26f78f8f6079bd3df34f4c9d
zhonghuiping <13750002982@163.com>
2021-11-28 12:06:23 +0800

    Initial commit

---
+++ index.tsx
@@ -0,0 +1,20 @@
+
+    /**
+    * This is an auto-generated demo by dumi
+    * if you think it is not working as expected,
+    * please report the issue at
+    * https://github.com/umijs/dumi/issues
+    **/
+    
+import React from 'react';
+import ReactDOM from 'react-dom';
+import '@antv/xflow/dist/index.css';
+import '@antv/x6/dist/x6.css';
+import '@antv/x6-react-components/dist/index.css';
+import 'antd/dist/antd.css';
+import App from './App';
+    
+ReactDOM.render(
+ <App />,
+document.getElementById('root'),
+);
